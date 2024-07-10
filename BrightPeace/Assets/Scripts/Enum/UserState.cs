35abbcc3e1dd2ef3dca2cbf7abc6d6e6bbd807e2
sinamonpie <44281451+sinamonpie@users.noreply.github.com>
2024-07-11 01:23:49 +0900

    포톤추가

---
+++ BrightPeace/Assets/Scripts/Enum/UserState.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum UserRole
+{
+    Security = 1,       //경비원
+    Patient = 2,        //환자
+    Mental = 2          //정신병자
+}
