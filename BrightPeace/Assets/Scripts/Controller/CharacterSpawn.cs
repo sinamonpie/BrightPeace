3348351daba3556d7e5a7a8e567b09df970e3d8f
Nextdoorbutter <zcake1113@naver.com>
2024-07-12 18:55:58 +0900

    캐릭터 스폰 re

--- BrightPeace/Assets/Scripts/Controller/CharacterSpawn.cs
+++ BrightPeace/Assets/Scripts/Controller/CharacterSpawn.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class CharacterSpawn : MonoBehaviour
 {
-    public GameObject spawnPointSet;
-    Transform spawnPoint;
+    public GameObject player1;
+    public GameObject player2;
+    public GameObject player3;
 
     private void Start()
     {
-        int spawnNum = Random.Range(0, 10);
+        SpawnCharacters();
+    }
 
-        spawnPoint = spawnPointSet.transform.GetChild(spawnNum);
+    private void SpawnCharacters()
+    {
+        List<int> spawnIndexes = GetUniqueSpawnIndexes(3); 
 
-        this.transform.position = spawnPoint.position;
+        Transform spawnPoint1 = transform.GetChild(spawnIndexes[0]);
+        player1.transform.position = spawnPoint1.position;
+
+        Transform spawnPoint2 = transform.GetChild(spawnIndexes[1]);
+        player2.transform.position = spawnPoint2.position;
+
+        Transform spawnPoint3 = transform.GetChild(spawnIndexes[2]);
+        player3.transform.position = spawnPoint3.position;
+    }
+
+    private List<int> GetUniqueSpawnIndexes(int count)
+    {
+        List<int> spawnIndexes = new List<int>();
+
+        while (spawnIndexes.Count < count)
+        {
+            int spawnNum = Random.Range(0, transform.childCount);
+            if (!spawnIndexes.Contains(spawnNum))
+            {
+                spawnIndexes.Add(spawnNum);
+            }
+        }
+
+        return spawnIndexes;
     }
 }
