598c6e11f4b9c2f32756f28b37eeec5c443f454c
new6ie <2fastboii@gmail.com>
2024-07-13 19:20:33 +0900

    리소스 프리팹화 + 이미지 추가

---
+++ BrightPeace/Assets/Scripts/Item/ItemObject/Radio.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Radio : Item
+{
+    public override void UseItem()
+    {
+        Debug.Log("Use Radio");
+    }
+}
