2b29d8da5a0b1c3b5ca66b75ffa593fd0196222c
Nextdoorbutter <zcake1113@naver.com>
2024-07-17 22:44:05 +0900

    e1

--- BrightPeace/Assets/Scripts/Object/EscapeEnding.cs
+++ BrightPeace/Assets/Scripts/Object/EscapeEnding.cs
@@ -4,13 +4,33 @@ using UnityEngine;
 
 public class EscapeEnding : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+    public GameObject triggerItem1;
+    public GameObject triggerItem2;
+    public GameObject triggerItem3;
 
-    void Update()
+    private bool trigger1 = false;
+    private bool trigger2 = false;
+    private bool trigger3 = false;
+
+    public bool EndigTriggerCheck(GameObject trigger)
     {
-        
+        if(trigger == triggerItem1)
+        {
+            trigger1 = true;
+        }
+        else if(trigger == triggerItem2)
+        {
+            trigger2 = true;
+        }
+        else if(trigger == triggerItem3)
+        {
+            trigger3 = true;
+        }
+
+        if(trigger1 && trigger2 && trigger3)
+        {
+            return true;
+        }
+        return false;
     }
 }
