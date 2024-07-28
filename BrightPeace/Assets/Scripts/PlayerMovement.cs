8d497c1bf5842b5241af777d85c0f2bd8e2040c4
YAROJ11 <jkswj1004@gamil.com>
2024-07-28 15:31:56 +0900

    grounded

--- BrightPeace/Assets/Scripts/PlayerMovement.cs
+++ BrightPeace/Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : NetworkBehaviour
     public CharacterController controller;
     public float PlayerSpeed = 2f;
 
+    private Vector3 velocity;
     public float Gravity = -9.81f;
 
     public Camera Camera;
@@ -41,11 +42,19 @@ public class PlayerMovement : NetworkBehaviour
             return;
         }
 
+        //Is Grounded
+        if (controller.isGrounded)
+        {
+            velocity = new Vector3(0, 0, 0);
+        }
+
         Quaternion cameraRotaionY = Quaternion.Euler(0, Camera.transform.rotation.eulerAngles.y, 0);
 
         Vector3 move = cameraRotaionY * new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical")) * Runner.DeltaTime * PlayerSpeed;
-   
-        controller.Move(move);
+
+        velocity.y += Gravity * Runner.DeltaTime;
+        
+        controller.Move(move + velocity * Runner.DeltaTime);
         
         if(move != Vector3.zero)
         {
