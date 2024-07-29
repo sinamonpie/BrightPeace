2dff348c4ff2d92e10c3ba9647a387aa0b3036fb
Nextdoorbutter <zcake1113@naver.com>
2024-07-29 14:48:59 +0900

    photonSpawn test

--- BrightPeace/Assets/Scripts/Object/EscapeEnding.cs
+++ BrightPeace/Assets/Scripts/Object/EscapeEnding.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class EscapeEnding : MonoBehaviour
 {
-    private bool crowBar_Trigger = false;
-    private bool driver_Trigger = false;
+    public bool crowBar_Trigger = false;
+    public bool driver_Trigger = false;
+    public bool TEST_Trigger = false;
 
     private bool endingTrigger = false;
 
