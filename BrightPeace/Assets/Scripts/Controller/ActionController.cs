88b84cd8271f1c96461c40b42e1024d3e9723900
Nextdoorbutter <zcake1113@naver.com>
2024-08-01 19:58:57 +0900

    캐비넷 1, 액션컨트롤러 플레이어변수 추가

--- BrightPeace/Assets/Scripts/Controller/ActionController.cs
+++ BrightPeace/Assets/Scripts/Controller/ActionController.cs
@@ -18,6 +18,7 @@ public class ActionController : MonoBehaviour
     public Inventory inventory;
 
     private bool canDoor = false;
+    public GameObject player;
 
     void Update()
     {
@@ -46,6 +47,12 @@ public class ActionController : MonoBehaviour
             {
                 canDoor = true;
                 EndigInfoAppear();
+            }
+
+            if (hitInfo.transform.tag == "Cabinet")
+            {
+                canDoor = true;
+                HideCabinetInfoAppear();
             }
 
         }
@@ -165,6 +172,24 @@ public class ActionController : MonoBehaviour
         actionText.gameObject.SetActive(true);
         actionText.text = "<color=yellow>" + " 탈출하기 " + "E Key" + "</color>";
         EndingAction();
+    }
+
+    public void HideCabinetAction()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (hitInfo.transform != null)
+            {
+                hitInfo.transform.GetComponent<CabinetController>().CabinetControl(player);
+            }
+        }
+    }
+
+    public void HideCabinetInfoAppear()
+    {
+        actionText.gameObject.SetActive(true);
+        actionText.text = "캐비넷에 숨기 " + "<color=yellow>" + "E키" + "</color>";
+        HideCabinetAction();
     }
 
 }
