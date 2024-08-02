dd9e89b5d4e9e36b0b25c04414fea6fdbb4db392
Nextdoorbutter <zcake1113@naver.com>
2024-08-02 13:31:12 +0900

    캐비넷2

--- BrightPeace/Assets/Scripts/Object/CabinetController.cs
+++ BrightPeace/Assets/Scripts/Object/CabinetController.cs
@@ -1,11 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CabinetController : MonoBehaviour
 {
+    private bool hideInCabinet = false;
+    Vector3 getOutCabinetPos;
+
     public void CabinetControl(GameObject player)
     {
-        player.transform.position = this.transform.position;
+        if (hideInCabinet)
+        {
+            player.transform.position = getOutCabinetPos;
+            hideInCabinet = false;
+        }
+        else
+        {
+            getOutCabinetPos = player.transform.position;
+            player.transform.position = this.transform.position;
+            hideInCabinet = true;
+        }
+    }
+
+    public bool HideInCabinet()
+    {
+        return hideInCabinet;
     }
 }
