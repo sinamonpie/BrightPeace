4c32492a8f9cfd859a801f1ca63a5246c9a14c31
YAROJ11 <jkswj1004@gamil.com>
2024-08-04 18:08:10 +0900

    1인칭

---
+++ BrightPeace/Assets/Scripts/JinScripts/PlayerController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    protected CameraControl cameracontrol;
+    
+    [SerializeField]
+    protected float moveSpeed = 2.0f;
+    protected float gravity = -9.81f;
+    protected Vector3 moveDirection;
+
+    [SerializeField]
+    protected Transform cameraTransform;
+    protected CharacterController characterController;
+}
