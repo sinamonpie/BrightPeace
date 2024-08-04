20f6874b3e7279b4bec0b763ed8c60c34ac98afc
new6ie <2fastboii@gmail.com>
2024-08-04 17:08:23 +0900

    머지용

--- BrightPeace/Assets/Scripts/Player/PlayerState.cs
+++ BrightPeace/Assets/Scripts/Player/PlayerState.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class PlayerState : MonoBehaviour
+public class PlayerState : MonoBehaviourPun
 {
     [SerializeField] int maxHp = 2;
     [SerializeField] int currentHp;
@@ -34,7 +35,20 @@ public class PlayerState : MonoBehaviour
     }
     public void TakeDamage(int damage)
     {
+        if (photonView.IsMine)
+        {
+            photonView.RPC("RPC_TakeDamage", RpcTarget.AllBuffered, damage);
+        }
+    }
+
+    [PunRPC]
+    void RPC_TakeDamage(int damage)
+    {
         currentHp -= damage;
+        if (currentHp <= 0)
+        {
+            // 플레이어가 죽었을 때 처리
+        }
     }
 
     public bool IsInCabinet()
