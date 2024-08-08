4673cd2db07cb4af10665ac6cee33c09f75d566e
Nextdoorbutter <zcake1113@naver.com>
2024-08-09 01:06:19 +0900

    퓨즈 1

---
+++ BrightPeace/Assets/Scripts/Object/PuseBox.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuseBox : MonoBehaviour
+{
+    private int puseNum = 0;
+
+    public void InsertPuse()
+    {
+        puseNum++;
+    }
+
+    public void CheckPuseNum()
+    {
+        if (puseNum == 3)
+        {
+            
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
